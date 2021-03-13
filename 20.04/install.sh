5aeb2f953098d7a38835ea524e96a8f8fc7c556e
DengSihan <root@dsh.email>
2021-03-13 09:42:29 +0000

    fix: add repository for php

--- 20.04/install.sh
+++ 20.04/install.sh
@@ -35,6 +35,8 @@ function install_node_npm {
 }
 
 function install_php {
+    sudo add-apt-repository -y ppa:ondrej/php
+    apt-get update
     apt-get install -y php7.4-bcmath php7.4-cli php7.4-curl php7.4-fpm php7.4-gd php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-pgsql php7.4-readline php7.4-xml php7.4-zip php7.4-sqlite3 php7.4-redis
 }
 
